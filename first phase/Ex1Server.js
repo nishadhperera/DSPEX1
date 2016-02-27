60e556e970cb03d210151982eeec021d67f535c2
nishadhperera <nishadhperera@gmail.com>
2016-02-27 10:51:55 +0200

    02/27 10:55

---
+++ first phase/Ex1Server.js
@@ -0,0 +1,33 @@
+﻿var http = require("http");
+var url = require("url");
+
+
+http.createServer(function(request, response){
+    response.writeHead(200, {"Content-Type":"text/plain", "Access-Control-Allow-Origin":"*"});
+
+    var params = url.parse(request.url,true).query;
+
+    var a = decodeURIComponent(params.arg1);
+    var b = decodeURIComponent(params.arg2);
+    var op = decodeURIComponent(params.op);
+
+    console.log(params);
+    var numA = new Number(a);
+    var numB = new Number(b);
+
+    if (op == "+") {
+        var r = new Number(numA + numB);
+    }else if (op=="-") {
+        var r = new Number(numA - numB);
+    }else if (op=="*") {
+        var r = new Number(numA * numB);
+    }else {
+        var r = new Number(numA / numB);
+    }
+
+    response.write(String(r));
+    response.end();
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
